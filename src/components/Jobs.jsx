58d39e134ffb9d86191f5cca5ac693f0925aafdb
student dsync <chetrii007@gmail.com>
2025-08-07 10:05:37 +0530

    made a simple change

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -156,7 +156,8 @@ export default function JobsSection() {
           Overseas
         </motion.h3>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
           {jobsOverseas.map((job, i) => (
             <JobCard key={i} job={job} />
           ))}
